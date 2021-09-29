84c5f80ad54e28895b2f31280023099c5b75f676
Abhinav <abhinavgujral33@gmail.com>
2021-09-29 19:29:24 +0530

    basic structure

---
+++ scripts/components/footer.js
@@ -0,0 +1,64 @@
+function footer(){
+
+    return ` <footer class="page_footer p-5 m-5 text-center">
+    <div class="row h5 mb-3">
+        <div class="col-3">Company</div>
+        <div class="col-3">Contact</div>
+        <div class="col-3">More</div>
+    </div>
+    <div class="row">
+        <div class="col-3">About</div>
+        <div class="col-3">Help/FAQ</div>
+        <div class="col-3">Airline Fees</div>
+    </div>
+    <div class="row">
+        <div class="col-3">Careers</div>
+        <div class="col-3">Press</div>
+        <div class="col-3">Airlines</div>
+    </div>
+    <div class="row">
+        <div class="col-3">Mobile</div>
+        <div class="col-3">Affiliates</div>
+        <div class="col-3">Low fare tips</div>
+    </div>
+    <div class="row">
+        <div class="col-3">Blog</div>
+        <div class="col-3">Hotel owners</div>
+    </div>
+    <div class="row">
+        <div class="col-3">How we work</div>
+        <div class="col-3">Partners</div>
+    </div>
+    <div class="row mb-5">
+        <div class="col-3"></div>
+        <div class="col-3">Advertise with us</div>
+    </div>
+    <div class="row textsize-12 mx-auto">
+        <div class="col-2 offset-2">Privacy</div>
+        <div class="col-2">Terms & Conditions</div>
+        <div class="col-2">©2021 KAYAK</div>
+
+    </div>
+    <div class="row">
+        <div class="col-12 text-center textsize-12 mx-auto"><span>Search flights, hotels, hire cars, travel guides and more
+                with KAYAK. KAYAK searches hundreds of other travel sites at once to get you the information you
+                need to make the right decisions. KAYAK also helps you find the right hotels for your needs.</span>
+        </div>
+    </div>
+    <div class="row m-3">
+        <div class="col-6 offset-2 text-center textsize-12 mx-auto">KAYAK is part of Booking Holdings Inc., the world leader
+            in online travel & related services.</div>
+    </div>
+    <div class="row">
+        <div class="col-12 text-center"><i class="fab fa-facebook-square m-2"></i><i class="fab fa-twitter-square m-2"></i><i class="fab fa-youtube m-2"></i><i class="fab fa-instagram-square m-2"></i></div>
+    
+    </div>
+    <div class="row">
+        <div class="col-12 text-center"><img src="./images/footer.JPG" alt=""></div>
+    </div>
+</footer>
+`
+
+}
+
+export default footer;
